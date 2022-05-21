5ba773f1470ea9a021eaf1ac011fff8b185ba68f
Stanislav <yustas@protonmail.com>
2022-05-22 00:18:58 +0500

    2.9.2. Сервис - Кинотеатр. Рефакторинг контроллера.

--- src/main/java/ru/job4j/cinema/controller/SessionController.java
+++ src/main/java/ru/job4j/cinema/controller/SessionController.java
@@ -6,13 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.cinema.model.Ticket;
+import ru.job4j.cinema.sevice.FilmsService;
 import ru.job4j.cinema.sevice.SessionService;
 import ru.job4j.cinema.sevice.TicketService;
 import ru.job4j.cinema.sevice.UserService;
 
 @ThreadSafe
 @Controller
-@RequestMapping("/order")
+@RequestMapping("/films")
 public class SessionController {
     private final SessionService sessionService;
 
@@ -20,10 +21,42 @@ public class SessionController {
 
     private final UserService userService;
 
+    private final FilmsService filmsService;
+
     @Autowired
-    public SessionController(SessionService sessionService, TicketService ticketService, UserService userService) {
+    public SessionController(SessionService sessionService, TicketService ticketService, UserService userService, FilmsService filmsService) {
         this.sessionService = sessionService;
         this.ticketService = ticketService;
         this.userService = userService;
+        this.filmsService = filmsService;
+    }
+
+    @GetMapping() // выбор фильма
+    public String films(Model model) {
+        model.addAttribute("films", filmsService.findAll());
+        return "films";
+    }
+
+    @GetMapping("/{filmId}") // выбор мест по id фильма
+    public String selectFilms(@PathVariable("filmId") int id, Model model) {
+        model.addAttribute("film", filmsService.findById(id));
+        model.addAttribute("rows", sessionService.rowsList());
+        model.addAttribute("cells", sessionService.cellsList());
+        return "/film";
+    }
+
+    @GetMapping("/formChoiceTicket") // форма выбора мест перед покупкой
+    public String choice(Model model) {
+        model.addAttribute("ticket", new Ticket(0, 0, 0, 0, 0));
+        return "/choiceTicket";
+    }
+
+    @PostMapping("/buyTicket") // покупка билета (страница отсутствует)
+    public String buy(@ModelAttribute Ticket ticket) {
+        if (sessionService.ticketSelection(ticket)) {
+            ticketService.add(ticket);
+            return "/successfully";
+        }
+        return "/failure";
     }
 }
